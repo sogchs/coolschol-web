09a8cf2114640f52efa2012df8bb7b71e971517c
sogchs <sogchs@gmail.com>
2019-03-22 14:14:18 +0100

    Login is running

---
+++ src/services/auth-service.js
@@ -0,0 +1,16 @@
+import http from './base-http-service';
+
+const login = (user) => http.post('/login', user)
+  .then(response => response.data);
+
+const register = (user) => http.post('/register', user)
+  .then(response => response.data);
+
+const logout = () => http.get('/logout')
+  .then(response => response.data);
+
+export default {
+  login,
+  register,
+  logout
+}
