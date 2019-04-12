fb136ec738e9fd0d1e7d4a48421541c378c3fa45
sogchs <sogchs@gmail.com>
2019-04-12 11:50:29 +0200

    Updates

---
+++ precache-manifest.3f549ac245c3b0a834719142ccaf52ee.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "24a77b8baa5b34fb50814225fae0272d",
+    "url": "/web-coolSchool/static/media/fa-brands-400.24a77b8b.woff2"
+  },
+  {
+    "revision": "044f05243f5817099d91",
+    "url": "/web-coolSchool/static/css/main.a8c2d6eb.chunk.css"
+  },
+  {
+    "revision": "b6267d5a52c8561a5dae",
+    "url": "/web-coolSchool/static/js/runtime~main.b5866e29.js"
+  },
+  {
+    "revision": "789486cd0c8f5ff7eb6affef843a0b1c",
+    "url": "/web-coolSchool/static/media/icomoon.789486cd.svg"
+  },
+  {
+    "revision": "dff349e6f162ed6024bd",
+    "url": "/web-coolSchool/static/js/2.0951da22.chunk.js"
+  },
+  {
+    "revision": "af625744350e68c90e943324af7e7c05",
+    "url": "/web-coolSchool/static/media/logo-coolSchool.af625744.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/web-coolSchool/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/web-coolSchool/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/web-coolSchool/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/web-coolSchool/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/web-coolSchool/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "34e0db45ef15f5a058149de318ca0641",
+    "url": "/web-coolSchool/static/media/fa-regular-400.34e0db45.woff2"
+  },
+  {
+    "revision": "c6e6643b688fe307651aa9374343d194",
+    "url": "/web-coolSchool/static/media/fa-regular-400.c6e6643b.woff"
+  },
+  {
+    "revision": "73e57f339e3e8574d0d4bc7a7c412d2a",
+    "url": "/web-coolSchool/static/media/fa-regular-400.73e57f33.eot"
+  },
+  {
+    "revision": "7599ecd0c99d05133229e0c4822b3086",
+    "url": "/web-coolSchool/static/media/fa-regular-400.7599ecd0.ttf"
+  },
+  {
+    "revision": "044f05243f5817099d91",
+    "url": "/web-coolSchool/static/js/main.8cc3b4ee.chunk.js"
+  },
+  {
+    "revision": "fb4ea3247aace8c1c8bcf904772a91c7",
+    "url": "/web-coolSchool/static/media/fa-solid-900.fb4ea324.woff2"
+  },
+  {
+    "revision": "175ef492f87a05e2ce45cb701d62c450",
+    "url": "/web-coolSchool/static/media/fa-brands-400.175ef492.woff"
+  },
+  {
+    "revision": "ecafd67cb64dc8f4d064abcf680dd924",
+    "url": "/web-coolSchool/static/media/fa-solid-900.ecafd67c.woff"
+  },
+  {
+    "revision": "28c2e91df5676f8a615bd3d519f0757b",
+    "url": "/web-coolSchool/static/media/fa-brands-400.28c2e91d.eot"
+  },
+  {
+    "revision": "8742c0293a67556fbb57133b56bb076e",
+    "url": "/web-coolSchool/static/media/fa-brands-400.8742c029.ttf"
+  },
+  {
+    "revision": "47758afd15884569fa987bb98ec2348f",
+    "url": "/web-coolSchool/static/media/fa-regular-400.47758afd.svg"
+  },
+  {
+    "revision": "cb45ce038f8d6ccf24a4307993230f8b",
+    "url": "/web-coolSchool/static/media/fa-solid-900.cb45ce03.eot"
+  },
+  {
+    "revision": "26fdf2ddc0e845cc454b72c97cdc705c",
+    "url": "/web-coolSchool/static/media/fa-solid-900.26fdf2dd.ttf"
+  },
+  {
+    "revision": "85dcebc2b9d56a2b94f79c8a7b731b00",
+    "url": "/web-coolSchool/static/media/fa-brands-400.85dcebc2.svg"
+  },
+  {
+    "revision": "e474ff3afacf902ab8f4dd43344f4f30",
+    "url": "/web-coolSchool/static/media/fa-solid-900.e474ff3a.svg"
+  },
+  {
+    "revision": "d17bca78cf794c7324528ade9cafa1c5",
+    "url": "/web-coolSchool/static/media/icomoon.d17bca78.ttf"
+  },
+  {
+    "revision": "db97a6dbb538bd3d46993729f1c120a4",
+    "url": "/web-coolSchool/static/media/icomoon.db97a6db.woff"
+  },
+  {
+    "revision": "eb34b2bc94a27c5472419e0293ec85db",
+    "url": "/web-coolSchool/static/media/icomoon.eb34b2bc.eot"
+  },
+  {
+    "revision": "dff349e6f162ed6024bd",
+    "url": "/web-coolSchool/static/css/2.d32aa70c.chunk.css"
+  },
+  {
+    "revision": "a1e033a96afb7258f5feff46d86bdc4f",
+    "url": "/web-coolSchool/index.html"
+  }
+];
